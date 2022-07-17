a412a1619e3d875b1c0521a3c7685fb9a570d9f4
furkannaktas <furkan.aktas95@gmail.com>
2022-07-17 17:22:00 -0400

    Constructor is created

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,10 +2,22 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Calculator {
+public class Calculator implements ActionListener {
 
+
+
+    Calculator(){
+
+    }
 
     public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
