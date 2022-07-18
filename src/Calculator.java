418b203c0f20d24c330098dd56a25ed3eed16bc7
furkannaktas <furkan.aktas95@gmail.com>
2022-07-17 20:56:52 -0400

    Numbers and functions are added to the panel

--- src/Calculator.java
+++ src/Calculator.java
@@ -70,6 +70,23 @@ public class Calculator implements ActionListener {
         panel.setLayout(new GridLayout(4,4,10,10));
         panel.setBackground(Color.lightGray);
 
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(multiplyButton);
+        panel.add(decimalButton);
+        panel.add(numberButtons[0]);
+        panel.add(equalButton);
+        panel.add(divideButton);
+
 
         frame.add(panel);
         frame.add(deleteButton);
